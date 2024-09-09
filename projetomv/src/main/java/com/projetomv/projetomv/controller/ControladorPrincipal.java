e4e32617b511112e8fe80a74e0b3715d21aa37d2
miller_marcelino <miller.marcelino@hotmail.com>
2024-09-09 12:52:50 -0300

    Adicionado o crud da classe contas

--- projetomv/src/main/java/com/projetomv/projetomv/controller/ControladorPrincipal.java
+++ projetomv/src/main/java/com/projetomv/projetomv/controller/ControladorPrincipal.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetomv.projetomv.dto.ClienteDto;
+import com.projetomv.projetomv.dto.ContaDto;
 import com.projetomv.projetomv.model.Cliente;
+import com.projetomv.projetomv.model.Conta;
 import com.projetomv.projetomv.repository.RepositoryCliente;
 import com.projetomv.projetomv.repository.RepositoryConta;
 import com.projetomv.projetomv.repository.RepositoryMovimentacao;
@@ -69,7 +71,7 @@ public class ControladorPrincipal {
     }
 
     @DeleteMapping("deletar-clientes/(id_cliente)")
-    public ResponseEntity<Long> deletarClientes (@PathVariable Long id_clientes) {
+    public ResponseEntity<Long> deletarClientes(@PathVariable Long id_clientes) {
         boolean existeClientes = repositoryCliente.existsById(id_clientes);
         if (existeClientes) {
             repositoryCliente.deleteById(id_clientes);
@@ -78,8 +80,9 @@ public class ControladorPrincipal {
         return new ResponseEntity<>(id_clientes, HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping ("atualizar-clientes/{id_clientes}")
-    public ResponseEntity<Cliente> atualizarClientes(@PathVariable Long id_clietes, @RequestBody ClienteDto clienteDto) {
+    @PutMapping("atualizar-clientes/{id_clientes}")
+    public ResponseEntity<Cliente> atualizarClientes(@PathVariable Long id_clietes,
+            @RequestBody ClienteDto clienteDto) {
         Optional<Cliente> existingClientes = repositoryCliente.findById(id_clietes);
 
         if (existingClientes.isPresent()) {
@@ -93,7 +96,72 @@ public class ControladorPrincipal {
 
             Cliente clienteatualizado = repositoryCliente.save(clientesExistente);
 
-            return ResponseEntity.ok(clienteatualizado);     
+            return ResponseEntity.ok(clienteatualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("cadastrar-contas")
+    private ResponseEntity<?> cadastrarcontas(@RequestBody @Valid ContaDto contaDto,
+            BindingResult bindingResult) {
+        try {
+            Conta conta = new Conta();
+
+            conta.setBanco(contaDto.getBanco());
+            conta.setCliente(contaDto.getCliente());
+            conta.setAgencia(contaDto.getAgencia());
+            conta.setNumero(contaDto.getNumero());
+
+            conta = repositoryConta.save(conta);
+
+            return new ResponseEntity<>("Conta Cadastrada com sucesso", HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao cadastrar conta" + e.getMessage());
+        }
+
+    }
+
+    @GetMapping(value = "listar-contas/{id_conta}")
+    public ResponseEntity<Conta> listarContas(@PathVariable Long id_conta) {
+        Optional<Conta> conta = repositoryConta.findById(id_conta);
+
+        if (conta.isPresent()) {
+            return ResponseEntity.ok(conta.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("deletar-contas/(id_contas)")
+    public ResponseEntity<Long> deletarContas(@PathVariable Long id_contas) {
+        boolean existeContas = repositoryConta.existsById(id_contas);
+        if (existeContas) {
+            repositoryConta.deleteById(id_contas);
+            return new ResponseEntity<>(id_contas, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(id_contas, HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("atualizar-contas/{id_contas}")
+    public ResponseEntity<Conta> atualizarContas(@PathVariable Long id_contas,
+            @RequestBody ContaDto contaDto) {
+        Optional<Conta> existingConta = repositoryConta.findById(id_contas);
+
+        if (existingConta.isPresent()) {
+
+            Conta contasExistente = existingConta.get();
+
+            contasExistente.setBanco(contaDto.getBanco());
+            contasExistente.setCliente(contaDto.getCliente());
+            contasExistente.setAgencia(contaDto.getAgencia());
+            contasExistente.setNumero(contaDto.getNumero());
+
+            Conta contaAtualizada = repositoryConta.save(contasExistente);
+
+            return ResponseEntity.ok(contaAtualizada);
         } else {
             return ResponseEntity.notFound().build();
         }
